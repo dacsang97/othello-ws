9b02c2c05844d64a8a3c3ad20d0b413ebbea2794
Evan Nguyen <dacsang97@gmail.com>
2018-11-12 11:32:17 +0700

    upload 2 git

---
+++ controller/game.js
@@ -0,0 +1,138 @@
+let io;
+let gameSocket;
+let rooms = [];
+
+exports.initGame = function(sio, socket) {
+  io = sio;
+  gameSocket = socket;
+  gameSocket.emit("connected", {
+    message: "You are connected!",
+    rooms
+  });
+
+  io.emit("updateListRooms", {
+    rooms
+  });
+
+  gameSocket.on("hostCreateNewGame", hostCreateNewGame);
+  gameSocket.on("disconnect", onDisconnect);
+  gameSocket.on("getInfo", () => {
+    gameSocket.emit("connected", {
+      message: "You are connected!",
+      rooms
+    });
+  });
+  // Player Events
+  gameSocket.on("playerJoinGame", playerJoinGame);
+  gameSocket.on("newMove", newMove);
+  gameSocket.on("surrender", surrender);
+  gameSocket.on("exit", exitRoom);
+  gameSocket.on("gameOver", gameOver);
+};
+
+function onDisconnect() {
+  rooms = rooms.filter(room => room.players[0].id !== this.id);
+  io.emit("updateListRooms", {
+    rooms
+  });
+}
+
+function exitRoom(data) {
+  io.sockets.in(data.gameId).emit("exit");
+}
+
+function surrender(data) {
+  const room = rooms.find(room => room.id === data.gameId);
+
+  io.sockets.in(data.gameId).emit("surrender", {
+    win: room.players[data.win - 1].name
+  });
+  io.sockets.clients(data.gameId).forEach(client => {
+    client.leave(data.gameId);
+  });
+}
+
+function hostCreateNewGame(data) {
+  // Create a unique Socket.IO Room
+  var thisGameId = (Math.random() * 10000000) | 0;
+
+  rooms.push({
+    id: thisGameId,
+    players: [
+      {
+        id: this.id,
+        name: data.name
+      }
+    ]
+  });
+
+  // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
+  this.emit("newGameCreated", {
+    gameId: thisGameId,
+    socketId: this.id,
+    ...data
+  });
+
+  // Join the Room and wait for the players
+  this.join(thisGameId.toString());
+  io.emit("updateListRooms", {
+    rooms
+  });
+}
+
+function playerJoinGame(data) {
+  console.log(
+    "Player " + data.name + " attempting to join game: " + data.gameId
+  );
+
+  // A reference to the player's Socket.IO socket object
+  // let sock = this
+
+  // If the room exists
+  let roomIds = rooms.map(room => room.id);
+  console.log(roomIds);
+  if (roomIds.includes(data.gameId)) {
+    console.log("Room is found.");
+    const room = rooms.find(room => room.id === data.gameId);
+    const playerNum = room.players.length;
+    const inRoom = room.players.map(player => player.id).includes(this.id);
+
+    if (inRoom) {
+      return this.emit("inRoom");
+    }
+
+    if (playerNum !== 2) {
+      this.join(data.gameId);
+      console.log(
+        "Player " + data.playerName + " joining game: " + data.gameId
+      );
+      const player = {
+        id: this.id,
+        name: data.name
+      };
+      room.players.push(player);
+      // remove all room this created
+      const listR = rooms.filter(room => room.players[0].id === this.id);
+      listR.forEach(room => {
+        this.leave(room.id);
+      });
+      rooms = rooms.filter(room => room.players[0].id !== this.id);
+      io.emit("updateListRooms", {
+        rooms
+      });
+      io.sockets.in(data.gameId).emit("playerJoinedRoom", room);
+    } else {
+      this.emit("errMess", { message: "This room is full" });
+    }
+  } else {
+    this.emit("errMess", { message: "This room does not exist." });
+  }
+}
+
+function newMove(data) {
+  console.log("Received Move with data: ");
+  console.log(data);
+  io.sockets.in(data.gameId).emit("newMove", data);
+}
+
+function gameOver(data) {}
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const { initGame } = require("./controller/game");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIO(server);
+server.listen(3000);
+
+io.sockets.on("connection", function(socket) {
+  initGame(io, socket);
+});
