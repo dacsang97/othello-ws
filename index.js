2b1c89ec289be6c3c62c14af8f1b58f96c7fe83b
Evan Nguyen <dacsang97@gmail.com>
2018-11-12 12:11:12 +0700

    fix origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { initGame } = require("./controller/game");
 const app = express();
 
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, { origins: "*:*" });
 server.listen(3000);
 
 io.sockets.on("connection", function(socket) {
